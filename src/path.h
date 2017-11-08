e6d71adddad79d1f5898f488c9e18f1aadec89e0
OriolOrra <oriolorraserra@gmail.com>
2017-11-08 14:08:59 +0100

    working? check again

--- src/path.h
+++ src/path.h
@@ -14,17 +14,58 @@ class Path{
     Path();
     ~Path();
 
+    bool getCircleTrajectoryXZBase(const geometry_msgs::Pose& startingPose,
+                          const double radius,
+                          const double startingAngleDeg,
+                          const double endAngleDeg,
+                          const double stepDeg,
+                          const int clockwise,
+                          std::vector<geometry_msgs::Pose> &waypoints);
+
+    bool getCircleTrajectoryYZBase(const geometry_msgs::Pose& startingPose,
+                          const double radius,
+                          const double startingAngleDeg,
+                          const double endAngleDeg,
+                          const double stepDeg,
+                          const int clockwise,
+                          std::vector<geometry_msgs::Pose> &waypoints);
+    bool getCircleTrajectoryXYBase(const geometry_msgs::Pose& startingPose,
+                          const double radius,
+                          const double startingAngleDeg,
+                          const double endAngleDeg,
+                          const double stepDeg,
+                          const int clockwise,
+                          std::vector<geometry_msgs::Pose> &waypoints);
+
     bool getCircleTrajectoryEEPlane(const geometry_msgs::Pose& startingPose,
                              const double radius,
                              const double startingAngleDeg,
                              const double endAngleDeg,
                              const double stepDeg,
-                             const int clockwise,       // 1,-1 -> clk-unclk
+                             const int clockwise,
                              std::vector<geometry_msgs::Pose> &waypoints);
+
+    bool getLineTrajectoryXBase(const geometry_msgs::Pose &startingPose,
+                       const double distance,
+                       const int direction,
+                       const double step,
+                       std::vector<geometry_msgs::Pose> &waypoints);
+
+    bool getLineTrajectoryYBase(const geometry_msgs::Pose &startingPose,
+                       const double distance,
+                       const int direction,
+                       const double step,
+                       std::vector<geometry_msgs::Pose> &waypoints);
+
+    bool getLineTrajectoryZBase(const geometry_msgs::Pose &startingPose,
+                       const double distance,
+                       const int direction,
+                       const double step,
+                       std::vector<geometry_msgs::Pose> &waypoints);
 
     bool getLineTrajectoryEEPlane(const geometry_msgs::Pose &startingPose,
                        const double distance,
-                       const int direction,   // 1,-1 -> x,-x     2,-2 -> y,-y        3,-3 -> z,-z
+                       const int direction,
                        const double step,
                        std::vector<geometry_msgs::Pose> &waypoints);
 };
--- src/ur5_move_group.cpp
+++ src/ur5_move_group.cpp
@@ -54,27 +54,20 @@ int main(int argc, char **argv)
 {
   Path path;
   bool success;
-
-  //Find path from this package
   std::string package_path = ros::package::getPath("p3_uvic");
 
-  //Load trajectory file to be read
   YAML::Node yaml_config  = YAML::LoadFile(package_path+"/config/trajectory.yaml");
 
-  //Get parameter Totalsize from YAML file
   int trajectories_size = yaml_config["Totalsize"].as<int>();
 
-  // Initialize trajectory waypoints array
   std::vector<geometry_msgs::Pose> waypoints;
-  waypoints.clear();
 
-  // Initialize ROS
   ros::init(argc, argv, "p3_uvic");
   ros::NodeHandle nh;
+
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  // MoveIt! configuration for UR5
   std::string moveit_group = yaml_config["MoveItGroup"].as<std::string>();
   moveit::planning_interface::MoveGroup group(yaml_config["MoveItGroup"].as<std::string>());
   moveit::planning_interface::MoveGroup::Plan my_plan;
@@ -82,78 +75,61 @@ int main(int argc, char **argv)
   group.setPlanningTime(10);
   group.setNumPlanningAttempts(5);
   group.setEndEffectorLink("ee_link");
- 
-  // set start state of the simulated robot to the current state
+
   group.setStartStateToCurrentState();
-
-  // start trajectory with the currrent state
   geometry_msgs::Pose startingPose = group.getCurrentPose().pose;
+  ROS_INFO_STREAM("Starting POSE: " << startingPose.position.x << "  " << startingPose.position.y << "  " << startingPose.position.z);
+  waypoints.clear();
 
-  //Loop for Trajectories
   for(int j=1; j <= trajectories_size; j++){
 
-    //Get all trajectory 
     YAML::Node trajectory_node = yaml_config["Trajectory" + std::to_string(j)];
-
-    //Get size of trajectory
-    int path_size = trajectory_node["size"].as<int>(); 
-
+    int path_size = trajectory_node["size"].as<int>();
     bool isPose = false;
 
-    //Loop for Paths
     for(int k = 1; k <= path_size; k++){
 
-      //Get path
       auto path_node = trajectory_node["Path" + std::to_string(k)];
-
-      //Get type of the path
       std::string path_type = path_node["Type"].as<std::string>();
 
       if(path_type.find("Circle") != std::string::npos){
 
-        // Read parameters from YAML
-        // Call 'getCircleTrajectoryEEPlane' function with the right parameters
-        
-        // Save waypoints
-        //...
+        double radius = path_node["Radius"].as<double>();
+        double start_angle = path_node["StartAngle"].as<double>();
+        double end_angle = path_node["EndAngle"].as<double>();
+        double step = path_node["AngleStep"].as<double>();
+        int clockwise = (start_angle >= end_angle ) ? -1: 1;
 
-        //Make 'statingPose', the last point of waypoints
-        //...
+        ROS_INFO_STREAM("CIRCLE: " << radius << "  " << start_angle << "  " << end_angle << "  " << step << " " << clockwise);
+        path.getCircleTrajectoryEEPlane(startingPose, radius, start_angle, end_angle, step, clockwise,  waypoints);
+
+        startingPose = waypoints.back();
 
       }else if (path_type.find("Line") != std::string::npos){
 
-        // Read parameters from YAML
-        // Call 'getLineTrajectoryEEPlane' function with the right parameters
-        
-        // Save waypoints
-        //...
+        double lenght = path_node["Lenght"].as<double>();
+        int direction = path_node["Direction"].as<double>();             //   1,-1 -> x,-x      2,-2 -> y,-y      3,-3 -> z,-z
+        double step = path_node["LenghtStep"].as<double>();
 
-        //Make 'statingPose', the last point of waypoints
-        //...
+        ROS_INFO_STREAM("LINE : " << lenght << "  " << direction << "  " << step );
+        path.getLineTrajectoryEEPlane(startingPose, lenght, direction, step, waypoints);
+
+        startingPose = waypoints.back();
 
       }else if (path_type.find("Pose") != std::string::npos){
         
-        // group.setNamedTarget(_name_of_the_pose_);
-        
-        //Move robot to Pose
+        group.setNamedTarget(path_node["Name"].as<std::string>());
         group.move();
-
-        //If the program enter here, should not execute 'executePath()' 
-        //....
+        isPose = true;
       }
     }
 
-    //Execute trajectory 
     if(!isPose) executePath(group, my_plan, waypoints, moveit_group);
 
-    //Update pose of the robot 
     group.setStartStateToCurrentState();
-    
-    //Get current state of the robot, to start next trajectory on the right place
     startingPose = group.getCurrentPose().pose;
-
     waypoints.clear();
-  
+    ROS_INFO_STREAM("Starting POSE: " << startingPose.position.x << "  " << startingPose.position.y << "  " << startingPose.position.z);
   }
 
   return 0;
